59faca588c72ee776f06460cc91aad5087c55855
Igor <iandrosov73@gmail.com>
2019-01-25 17:19:32 -0500

    Add intent and sentiment componets

---
+++ lwc-language/main/default/lwc/textIntent/textIntent.js
@@ -0,0 +1,26 @@
+import { LightningElement, track } from 'lwc';
+import findTextIntent from '@salesforce/apex/LWCL_LangController.findTextIntent';
+
+export default class TextIntent extends LightningElement {
+    @track document = '';
+    @track token = '';
+    @track model = '';
+    @track textEval;
+
+    handleDocumentChange(event){
+        this.document = event.target.value;
+    }
+    handleTokenChange(event){
+        this.token = event.target.value;
+    }
+    handleModelChange(event){
+        this.model = event.target.value;
+    }
+    handleTextEvaluation(){
+        findTextIntent({ document: this.document, access_token: this.token, model: this.model })
+            .then(result => {
+                this.textEval = result;
+                //this.error = undefined;
+            });
+    }
+}
